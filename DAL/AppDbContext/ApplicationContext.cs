627feb13f145a0531657035a004d6fefbb9fe025
amarko1 <amarko1@racunarstvo.hr>
2025-03-26 22:33:05 +0100

    jos jedan

--- DAL/AppDbContext/ApplicationContext.cs
+++ DAL/AppDbContext/ApplicationContext.cs
@@ -141,27 +141,6 @@ namespace DAL.AppDbContext
                 .Property(oi => oi.CakeName)
                 .IsRequired();
 
-            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-            {
-                foreach (var property in entityType.GetProperties())
-                {
-                    if (property.ClrType == typeof(DateTime))
-                    {
-                        property.SetValueConverter(new ValueConverter<DateTime, DateTime>(
-                            v => v.Kind == DateTimeKind.Utc ? v : v.ToUniversalTime(),
-                            v => DateTime.SpecifyKind(v, DateTimeKind.Utc)));
-                    }
-
-                    if (property.ClrType == typeof(DateTime?))
-                    {
-                        property.SetValueConverter(new ValueConverter<DateTime?, DateTime?>(
-                            v => v.HasValue ? (v.Value.Kind == DateTimeKind.Utc ? v.Value : v.Value.ToUniversalTime()) : v,
-                            v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v));
-                    }
-                }
-            }
-
-
             base.OnModelCreating(modelBuilder);
         }
     }
